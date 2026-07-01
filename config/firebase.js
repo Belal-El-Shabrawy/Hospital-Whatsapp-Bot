510a61fc548b7fcc01a261bedfd0e2c90ee5a35b
Belal El-Shabrawy <madbusiness05@gmail.com>
2026-07-01 17:05:52 +0300

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -1,7 +1,15 @@
 // إعداد الاتصال بـ Firebase Admin (Firestore)
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
-const serviceAccount = require('../firebase-key.json');
+let serviceAccount;
+
+if (process.env.FIREBASE_CREDENTIALS) {
+    // 1. لو إحنا على سيرفر Render، هيقرأ المفتاح من الـ Environment Variables
+    serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS);
+} else {
+    // 2. لو إحنا على جهازك (Local)، هيقرأ الملف بتاعك عادي جداً
+    serviceAccount = require('../firebase-key.json');
+}
 
 initializeApp({
     credential: cert(serviceAccount)
